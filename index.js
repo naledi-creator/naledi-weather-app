162d16bcb628ade7b435b4c907092e39ad402a02
naledi-creator <naledimokgotho@gmail.com>
2024-10-12 19:39:08 +0200

    API added

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector(".type-box");
-  let cityElement = document.querySelector("h1");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 let searchForElement = document.querySelector("#search-form");
 searchForElement.addEventListener("submit", searchSubmit);
+
+function reloadWeather(response) {
+  let temperatureElement = document.querySelector(".temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("h1");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "2188ffc414b4604d7f7e53b6abt0o00c";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(reloadWeather);
+}
