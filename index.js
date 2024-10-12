d96d798a1626472089f95ec2a952dc9f3c0e1107
naledi-creator <naledimokgotho@gmail.com>
2024-10-12 21:27:34 +0200

    updated weather data

--- index.js
+++ index.js
@@ -10,13 +10,37 @@ function reloadWeather(response) {
   let temperatureElement = document.querySelector(".temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("h1");
+  let descriptionElement = document.querySelector("#weather-description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind");
+  let date = new Date(response.date.time * 1000);
+  let timeElement = document.querySelector("#time");
+  let iconElement = document.querySelector(".icon");
 
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
+  timeElement.innerHTML = formatDate(date);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
 
 function searchCity(city) {
   let apiKey = "2188ffc414b4604d7f7e53b6abt0o00c";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(reloadWeather);
+}
+searchCity("Paris");
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Friday", "Saturday"];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0 ${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
